1497c1c2d9dc8a895dff5ef3784c663be8e79248
KEI20030212 <tkey4392@gmail.com>
2026-06-20 23:40:47 +0900

    Add files via upload

--- views/home.py
+++ views/home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from views.board import render_home_page
-from views.attendance_check import render_attendance_seat_page
+#from views.attendance_check import render_attendance_seat_page
 from views.messages import render_messages_page
 
 def render_combined_home_page():
@@ -9,7 +9,7 @@ def render_combined_home_page():
 
     record_type = st.radio(
         "確認事項を選択してください", 
-        ["📢 ホーム・連絡掲示板", "🗺️ 本日の教室状況・座席管理", "💌 あなた宛てのメッセージ"], 
+        ["📢 ホーム・連絡掲示板", "💌 あなた宛てのメッセージ"], 
         horizontal=True, 
         key="record_type_combined"
     )
@@ -17,7 +17,7 @@ def render_combined_home_page():
 
     if record_type == "📢 ホーム・連絡掲示板":
         render_home_page()
-    elif record_type == "🗺️ 本日の教室状況・座席管理":
-        render_attendance_seat_page()    
+    #elif record_type == "🗺️ 本日の教室状況・座席管理":
+        #render_attendance_seat_page()    
     elif record_type == "💌 あなた宛てのメッセージ":
         render_messages_page()
