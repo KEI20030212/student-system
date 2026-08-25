f9e3ab74ecef2f8b045bd7e934c5bbcf95065ea1
KEI20030212 <tkey4392@gmail.com>
2026-08-26 00:45:45 +0900

    Add files via upload

--- views/home.py
+++ views/home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from views.board import render_home_page
 #from views.attendance_check import render_attendance_seat_page
-from views.feedback_all_board.py import render_feedback_board
+from views.feedback_all_board.py import render_feedback_all_board
 
 def render_combined_home_page():
     st.header("📢 ホーム")
@@ -20,4 +20,4 @@ def render_combined_home_page():
     #elif record_type == "🗺️ 本日の教室状況・座席管理":
         #render_attendance_seat_page()    
     elif record_type == "📩 フィードバック一覧":
-        render_feedback_board()
+        render_feedback_all_board()
